68f1912683ad2deb3a92a167bab75fa7c79f03f5
iamjackson7 <99736392+iamjackson7@users.noreply.github.com>
2022-03-03 15:12:59 -0500

    Update Breack.py

--- Tomando-decisiones/Breack.py
+++ Tomando-decisiones/Breack.py
@@ -1,1 +1,7 @@
-
+#Crear un programa que cuando encuentre la letra "b", pare.
+cadena = 'programacion'
+for letra in cadena:
+    if letra == 'b':
+        print("Se encontró la b")
+        break
+    print(letra)
