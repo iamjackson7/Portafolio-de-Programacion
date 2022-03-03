c07f30b5501a3626e2cc575488d284fccb835523
iamjackson7 <99736392+iamjackson7@users.noreply.github.com>
2022-03-03 13:43:17 -0500

    Update tuple.py

--- Casting-en-python/tuple.py
+++ Casting-en-python/tuple.py
@@ -1,1 +1,15 @@
-
+#Escribir un programa que muestre la lista de numeros, separados por comas.
+ejemplo = input("Introduce una muestra de números separados por comas: ")
+ejemplo = ejemplo.split(',')
+n = len(ejemplo)
+for i in range(n):
+    ejemplo[i] = int(ejemplo[i])
+ejemplo = tuple(ejemplo)
+sum = 0
+sumsq = 0
+for i in ejemplo:
+    sum += i
+    sumsq += i**2
+mean = sum/n
+stdev = (sumsq/n-mean**2)**(1/2)
+print('La media es', mean, ', y la desviación típica es', stdev)
