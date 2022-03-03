cccd5f6e0ee7d93fe5b6c72dc2ab3575d3e8aaf6
iamjackson7 <99736392+iamjackson7@users.noreply.github.com>
2022-03-03 15:24:40 -0500

    Update Breack.py

--- Tomando-decisiones/Breack.py
+++ Tomando-decisiones/Breack.py
@@ -1,7 +1,28 @@
-#Crear un programa que cuando encuentre la letra "b", pare.
+1.#Crear un programa que cuando encuentre la letra "b", pare.
 cadena = 'programacion'
 for letra in cadena:
     if letra == 'b':
         print("Se encontró la b")
         break
     print(letra)
+
+2.#Crear un programa que cuando llegue al 10 pare.
+j=0
+for i in range(15):
+    j+=2
+    print("i:",i,"j:",j)
+    if j==10:
+        break
+        
+3.
+contandor=0
+for i in range(10):
+    for j in range(10):
+        contandor += 1
+        print(i,j)
+        break
+#Nunca se realiza más de una iteración
+#El break no afecta a este for
+print("contandor:",contandor)
+ 
+
