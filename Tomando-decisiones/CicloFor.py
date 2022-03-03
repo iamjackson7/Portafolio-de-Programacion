8c7d431ae9bd7780e584bcc40937ef19db910976
iamjackson7 <99736392+iamjackson7@users.noreply.github.com>
2022-03-03 15:26:55 -0500

    Update CicloFor.py

--- Tomando-decisiones/CicloFor.py
+++ Tomando-decisiones/CicloFor.py
@@ -1,4 +1,4 @@
-# Calcular el promedio de un alumno que tiene 8 calificaciones en la materia de Cálculo.
+1.# Calcular el promedio de un alumno que tiene 8 calificaciones en la materia de Cálculo.
 
 n = 8
 suma = 0
@@ -8,3 +8,27 @@ for i in range(n):
 
 promedio = suma/n
 print('Promedio:', promedio)
+
+2.# Leer 20 números y obtener su cubo y su cuarta.
+
+n = 20
+for i in range(n):
+    num = int(input('Ingrese numero: '))
+    print('Cubo:', num**3)
+    print('Cuarta:', num**4)
+
+3.# Leer 5 números e imprimir cuantos 
+positivos = 0; negativos =0;neutros=0
+
+for i in range(5):
+    num = int(input('Ingrese numero: '))
+    if num > 0:
+        positivos += 1
+    elif num < 0:
+        negativos += 1
+    else:
+        neutros += 1
+
+print('Total positivos:', positivos)
+print('Total negativos:', negativos)
+print('Total neutros:', neutros)
