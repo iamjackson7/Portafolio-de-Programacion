71e1ef918e5b8de1a582a16f67ea0e364f337de0
iamjackson7 <99736392+iamjackson7@users.noreply.github.com>
2022-03-03 15:15:12 -0500

    Update CicloWhile.py

--- Tomando-decisiones/CicloWhile.py
+++ Tomando-decisiones/CicloWhile.py
@@ -1,1 +1,35 @@
+#Crear un menu donde se pueda elejir multiples opciones y hacer las operaciones basicas.
+
+op = -1
+num1 = 0
+num2 = 0
+
+while op != 0:
+
+
+    print('<1> sumar')
+    print('<2> restar')
+    print('<3> multiplicar')
+    print('<4> dividir')
+    print('<5> salir')
+
+    op = int(input('Ingrese opcion:'))
+
+    if op != 0 :
+        num1 = int(input('Ingresar numero 1:'))
+        num2 = int(input('Ingresar numero 2:'))
+    
+    if op ==1 :
+        print('suma:',num1+num2)
+    elif op ==2 :
+        print('resta:',num1-num2)
+    elif op ==3 :
+        print('multiplicacion:', num1*num2)
+    elif op == 4 :
+        print('division:',num1/num2)
+    else :
+        print ('no existe la opcion.')
+© 2022 GitHub, Inc.
+Terms
+Privacy
 
