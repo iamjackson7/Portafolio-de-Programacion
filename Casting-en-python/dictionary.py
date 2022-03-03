0444729c90d05a8b12b7ad8deeb383d61737da61
iamjackson7 <99736392+iamjackson7@users.noreply.github.com>
2022-03-03 13:39:47 -0500

    Update dictionary.py

--- Casting-en-python/dictionary.py
+++ Casting-en-python/dictionary.py
@@ -1,1 +1,9 @@
+my_diccionario = {'nombre': 'Omar', 'edad': 19}
 
+    print(mi_diccionario['nombre'])
+    
+    print(mi_diccionario.get('edad'))
+    
+    print(mi_diccionario.get('dirección'))
+    
+    print(mi_diccionario['dirreción'])
