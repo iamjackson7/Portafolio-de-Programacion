d4c0299a0e383fc467bad980a2b896dcd8301dba
iamjackson7 <99736392+iamjackson7@users.noreply.github.com>
2022-03-03 15:17:41 -0500

    Update sentenciaif.py

--- Tomando-decisiones/sentenciaif.py
+++ Tomando-decisiones/sentenciaif.py
@@ -1,1 +1,6 @@
-
+#Escribir un programa que solicite un valor entero al usuario y determine si es positivo o negativo.
+num1=int(input("Ingrese un numero Entero:"))
+if num1>0:
+    print("El numero es positivo")
+else:
+    print("El numero es negativo")
