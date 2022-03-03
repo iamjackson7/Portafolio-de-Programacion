06a3994b87d8ecbf5ceff5e7c82ccb7ccba0237d
iamjackson7 <99736392+iamjackson7@users.noreply.github.com>
2022-03-03 15:14:19 -0500

    Update CicloFor.py

--- Tomando-decisiones/CicloFor.py
+++ Tomando-decisiones/CicloFor.py
@@ -1,1 +1,10 @@
+# Calcular el promedio de un alumno que tiene 8 calificaciones en la materia de Cálculo.
 
+n = 8
+suma = 0
+for i in range(n):
+    nota = float(input('Ingrese nota ' + str(i) + ': '))
+    suma = suma + nota
+
+promedio = suma/n
+print('Promedio:', promedio)
