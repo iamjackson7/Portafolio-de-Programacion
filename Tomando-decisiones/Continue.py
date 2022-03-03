e997a63e63f4a579bf6bd10ae95096cc0b306b12
iamjackson7 <99736392+iamjackson7@users.noreply.github.com>
2022-03-03 15:29:33 -0500

    Update Continue.py

--- Tomando-decisiones/Continue.py
+++ Tomando-decisiones/Continue.py
@@ -1,6 +1,24 @@
+1.
 x = 7
 while x > 0:
     x -= 1
     if x == 3:
         continue
     print(x)
+
+2.
+cadena = 'Python'
+for letra in cadena:
+    if letra == 'o':
+        continue
+    print(letra)
+    print("..........")
+    
+3.
+a=0
+for i in range(10):
+    a+=2
+    print("i:",i,"a:",a)
+    if a>=2 and  a<=18:
+        continue
+    print("el valor de a:",a)
